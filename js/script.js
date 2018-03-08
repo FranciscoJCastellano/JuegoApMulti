089860e63ddd6eba4f2e9f55206944d954bef545
FranciscoJCastellano <l04sd406@aig.uc3m.es>
2018-03-08 14:12:35 +0100

    El jugador se mueve, se borra mal el canvas: queda rastro

--- js/script.js
+++ js/script.js
@@ -18,26 +18,34 @@ function cargaContextoCanvas(idCanvas){
   return 0;
 }
 function gameOn(){
-  setInterval('draw',5);
-}
-function setup(){
-  player=new Player();
   player.show();
+  setInterval(draw,20);
 }
+/*Guardamos el contexto sin nada y dibujamos al jugadxr en la posicion inicial*/
+function setup(){
+  ctx = cargaContextoCanvas('myCanvas');
+  if(ctx){
+    //console.log("save");
+    ctx.save();  // guarda el contexto limpio de efectos
+    player=new Player();
+    player.show();
+  }
+}
+/*borramos el canvas y dibujamos de nuevo al player*/
 function draw(){
   borra_todo();
   ctx = cargaContextoCanvas('myCanvas');
   if(ctx){
-    console.log("save");
+    //console.log("save");
     ctx.save();  // guarda el contexto limpio de efectos
-    pelota.update();
+    player.update();
   }
 }
 
 function borra_todo(){
   ctx = cargaContextoCanvas('myCanvas');
   if(ctx){
-    console.log("restore");
+    //console.log("restore");
     ctx.restore();              // restaura el contexto sin efectos
     ctx.clearRect(0,0,800,500); // borra las figuras
     ctx.save();                 // guarda el contexto limpio de efectos
