d1fae873d528d16b958ab8cb8b610a43fec76449
JaimeQuin <100329996@alumnos.uc3m.es>
2018-03-14 14:59:29 +0100

    Añado video.js, video_style.css y una prueba del vídeo en la seccion REFERENCIAS, que no influye a la seccion juego

--- js/video.js
+++ js/video.js
@@ -1,2 +1,29 @@
-/*archivo para implementar funciones relacionadas con controlar el juego desde el video*/
-/*importante para subir el nivel cuando se sobreviva a la noche con el video y se haga de dia*/
+/************************************
+Autor: Jaime Moreno Quintanar
+Fecha: 14/03/2018
+Definición: Archivo para implementar funciones relacionadas con controlar el juego desde el video
+importante para subir el nivel cuando se sobreviva a la noche con el video y se haga de dia
+Referencias: https://www.w3schools.com/tags/ref_av_dom.asp
+*************************************/
+var videoIsEnded = 0; //Si 1 --> vídeo terminado. Si 0 --> video no terminado
+
+function alerta (texto) {
+	console.log("Evento Capturado: " + texto);
+}
+
+function init(){
+	document.video = document.getElementById("video");
+}
+document.addEventListener("DOMContentLoaded", init, false);
+
+function getVideo() {
+	return document.video;
+}
+function isEnded(video){
+	if (video.ended == true){
+		console.log("Ended");
+		videoIsEnded = 1;
+	}else {
+		console.log("Not ended");
+	}
+}
